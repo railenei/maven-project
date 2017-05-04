c9afc191bc5ebc56d70ef7159da9e407db351ef5
railenei <radu.ailenei@quantrio.ro>
2017-05-04 09:24:09 +0300

    Removed error

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,6 +18,6 @@ public class Greeter {
 */
 
   public final  String greet(final String someone) {
-    return1 String.format("Hello, %s!", someone);
+    return String.format("Hello, %s!", someone);
   }
 }
